2d1cce5976731094b898e5f89951ae40fb9cee87
menbou <maimai.menboumenbou.2016@gmail.com>
2017-05-19 19:16:59 +0900

    add new file

---
+++ Illumiball/Assets/Script/GravityController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+	const float Gravity = 9.81F;
+
+	public float gravityScale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vector = new Vector3 ();
+
+		if (Application.isEditor) {
+
+
+			vector.x = Input.GetAxis ("Horizontal");
+			vector.z = Input.GetAxis ("Vertical");
+
+			if (Input.GetKey ("z")) {
+				vector.y = 1.0f;
+			} else {
+				vector.y = -1.0f;
+			}
+		} else if(Application.platform == RuntimePlatform.WindowsPlayer) {
+			Debug.Log("This is a debug build!");
+			vector.x = Input.GetAxis ("Horizontal");
+			vector.z = Input.GetAxis ("Vertical");
+
+			if (Input.GetKey ("z")) {
+				vector.y = 1.0f;
+			} else {
+				vector.y = -1.0f;
+			}
+		} else {
+			vector.x = Input.acceleration.x;
+			vector.y = Input.acceleration.y;
+			vector.z = Input.acceleration.z;
+		}
+
+		Physics.gravity = Gravity * vector.normalized * gravityScale;
+
+	}
+}
